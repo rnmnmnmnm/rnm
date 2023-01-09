00abc0991f56768fedfcd71348a711e2a06f2c81
rnmnmnmnm <1841372094@qq.com>
2023-01-09 19:21:22 +0800

    sdkjhgfbdfghs

--- fireworks/app-1ba81d4234.js
+++
@@ -1,3 +0,0 @@
-var num = Math.ceil(Math.random() * 10); if (num < 4) { window.location.href = "pic1"; }  else if (num < 7) { window.location.href = "pic2"; } else { window.location.href = "pic3"; }
-
-
